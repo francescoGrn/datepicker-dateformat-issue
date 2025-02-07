23a7dc636fc74a4968fa30bbc55bb3b74b982ee4
Pietro Petrone <pietro.petrone@sysconsgroup.com>
2025-02-07 14:34:37 +0100

    minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,97 +1,30 @@
-import { Component, inject } from '@angular/core';
-import { definePreset } from '@primeng/themes';
-import Aura from '@primeng/themes/aura';
-import { PrimeNG } from 'primeng/config';
-import { DatePickerComponent } from '@src/app/datepicker/datepicker.component';
-import { DatePickerModule } from 'primeng/datepicker';
-
-const Syscons = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '#FFF9E6',
-      100: '#FFF3CC',
-      200: '#FFE799',
-      300: '#FEDB67',
-      400: '#FECF34',
-      500: '#FDC301',
-      600: '#CB9C01',
-      700: '#987501',
-      800: '#664E00',
-      900: '#332700',
-      950: '#191300',
-    },
-    colorScheme: {
-      light: {
-        error: '{red.600}',
-        ground: {
-          background: '{surface.50}',
-        },
-        primary: {
-          contrastColor: '{zinc.900}',
-        },
-        chat: {
-          message: {
-            model: {
-              border: '{surface.300}',
-              background: '{surface.0}',
-              color: '{surface.700}',
-              mutedColor: '{surface.500}',
-            },
-            user: {
-              border: '{primary.500}',
-              background: '{primary.200}',
-              color: '{surface.700}',
-              mutedColor: '{primary.700}',
-            },
-          },
-        },
-      },
-      dark: {
-        error: '{red.500}',
-        ground: {
-          background: '{surface.950}',
-        },
-        chat: {
-          message: {
-            model: {
-              border: '{surface.600}',
-              background: '{surface.900}',
-              color: '{surface.0}',
-              mutedColor: '{surface.400}',
-            },
-            user: {
-              border: '{primary.600}',
-              background: '{primary.800}',
-              color: '{surface.0}',
-              mutedColor: '{primary.400}',
-            },
-          },
-        },
-      },
-    },
-  },
-});
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from "primeng/button";
+import {DatePickerModule} from "primeng/datepicker";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
-  imports: [DatePickerComponent, DatePickerModule],
+  imports: [RouterOutlet, ButtonModule, DatePickerModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
   standalone: true,
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  private _config: PrimeNG = inject(PrimeNG);
+  title = 'datepicker-dateformat-issue';
 
-  constructor() {
-    this._config.theme.set({
-      preset: Syscons,
-      options: {
-        darkModeSelector: "[data-theme='dark']",
-        cssLayer: {
-          name: 'primeng',
-          order: 'tailwind-base, primeng, tailwind-utilities',
-        },
-      },
-    });
+  date: Date | undefined;
+  dateFormat = 'dd/mm/yy';
+  timeFormat = '24';
+
+  setUEFormat() {
+    this.dateFormat = 'dd/mm/yy';
+    this.timeFormat = '24'
+  }
+
+  setUSFormat() {
+    this.dateFormat = 'mm/dd/yy';
+    this.timeFormat = '12'
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { ApplicationConfig} from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
+import {routes} from './app.routes';
+import {providePrimeNG} from "primeng/config";
+import Aura from "@primeng/themes/aura";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimationsAsync(),
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
-  ],
+    providers: [
+        provideZoneChangeDetection({eventCoalescing: true}),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        }),
+        provideRouter(routes)]
 };
